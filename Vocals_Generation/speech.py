ee999b807e60501af88a0d56afb238e913cfa065
balaji <vasappagaribalajigowd@gmail.com>
2026-04-26 21:32:27 +0530

    "Modified Code"

--- Vocals_Generation/speech.py
+++ Vocals_Generation/speech.py
@@ -1,12 +1,8 @@
 import pyttsx3
-import threading
 
 engine = pyttsx3.init()
 
 def speak(text):
+    engine.stop()  # stop any previous speech
     engine.say(text)
     engine.runAndWait()
-
-
-def speak_async(text):
-    threading.Thread(target=speak, args=(text,), daemon=True).start()
